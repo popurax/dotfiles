b3f26f3984b327bc373f23cb52e6962973dd50c5
OTOTOTO <OTOTOTO@otot.ot>
2019-07-20 20:44:23 +0900

    refactor: いらないコードを消す

--- install.py
+++ install.py
@@ -1,42 +1,30 @@
+#!/usr/bin/python3
 import platform
-from pprint import pprint
 import logging
-import random
 from collections import defaultdict
 import os
 from os import path
-from enum import Enum
 
 messageFormat = "[%(who)s %(count)s回目] %(message)s"
 formatter = logging.Formatter(messageFormat)
 stdHandler = logging.StreamHandler()
 stdHandler.setFormatter(formatter)
-
-logging.basicConfig(level=logging.DEBUG,handlers=[stdHandler])
-
+logging.basicConfig(level=logging.DEBUG, handlers=[stdHandler])
 class ExtensionLogRecord(logging.LogRecord):
         counts = defaultdict(int)
-
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             msg = str(self.msg)
             self.counts[msg] += 1
             self.who = platform.uname().node
-            self.cost = random.randint(1, 100)
             self.count = self.counts[msg]
-
 logging.setLogRecordFactory(ExtensionLogRecord)
 log = logging.getLogger(__name__)
 
-# Enum("FileType", "POWERSHELL SHELLSCRIPT")と書くよりこっちのほうが補完が効くのでミスが無い
-class FileType(Enum):
-    POWERSHELL=1
-    SHELLSCRIPT=2
-
 ignoreFiles = [
-path.basename(__file__),
-".git",
-".vscode",
+    path.basename(__file__),
+    ".git",
+    ".vscode",
 ]
 
 currentDir = path.dirname(path.abspath(__file__))
@@ -44,7 +32,8 @@ currentDir = path.dirname(path.abspath(__file__))
 def main():
     pf = platform.system()
     if(pf == 'Windows'):
-        pathProfile = os.environ["USERPROFILE"] + "\Documents\WindowsPowerShell"
+        pathProfile = os.environ["USERPROFILE"] + \
+            "\Documents\WindowsPowerShell"
         files = [i for i in os.listdir(".") if not i in ignoreFiles]
         ps1s = [i for i in files if i.split(".")[-1] == "ps1"]
         # 戻り値が__NoneType__で例外処理とか増えていきそうなので、for句を使う。
@@ -53,14 +42,15 @@ def main():
             if not path.isdir(pathProfile):
                 os.mkdir(pathProfile)
             log.debug(currentDir)
-            os.symlink(path.join(currentDir,i), path.join(pathProfile, i))
+            os.symlink(path.join(currentDir, i), path.join(pathProfile, i))
     elif(pf == 'Linux'):
         pathProfile = os.environ["HOME"]
         log.info(pf)
         files = [i for i in os.listdir(".") if not i in ignoreFiles]
         shellScripts = [i for i in files if not i.split(".")[-1] == "ps1"]
         for i in shellScripts:
-            os.symlink(path.join(currentDir,i), path.join(pathProfile, i))
+            os.symlink(path.join(currentDir, i), path.join(pathProfile, i))
+
 
 if __name__ == "__main__":
     main()

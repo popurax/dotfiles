0b22c232144fee2f87b91f6284220f82e4a32295
popurax <horumon3939@gmail.com>
2020-02-10 12:53:19 +0900

    fix: ファイルとパスの指定方法が今後複雑化する書き方になっていた為、"リストオブジェクト化して反復処理"に変更。 feat: VSCodeのスニペットを追加。

--- install.py
+++ install.py
@@ -3,14 +3,13 @@ import platform
 import logging
 from collections import defaultdict
 import os
-from os import path
-
+import enum
 
 messageFormat = "[%(who)s %(count)s回目] %(message)s"
 formatter = logging.Formatter(messageFormat)
 stdHandler = logging.StreamHandler()
 stdHandler.setFormatter(formatter)
-logging.basicConfig(level=logging.INFO, handlers=[stdHandler])
+logging.basicConfig(level=logging.DEBUG, handlers=[stdHandler])
 class ExtensionLogRecord(logging.LogRecord):
         counts = defaultdict(int)
         def __init__(self, *args, **kwargs):
@@ -22,35 +21,58 @@ class ExtensionLogRecord(logging.LogRecord):
 logging.setLogRecordFactory(ExtensionLogRecord)
 log = logging.getLogger(__name__)
 
-ignoreFiles = [
-    path.basename(__file__),
-    ".git",
-    ".vscode",
+# os.environ["HOME"]はwindowsに無いので例外が発生する。
+# 他のOS環境でも同様の問題が起こりそうなので、ラムダ関数に変更し遅延評価する。
+# （文字列にしてeval関数で実行する方法もあるが、補完やIDEアシスト機能が効かなくなるので使いたくない。）
+targetFiles = [
+    {
+        'fileName': 'javascript.json', 
+        'windowsPathDir': lambda: os.environ["USERPROFILE"] + "\\AppData\\Roaming\\Code\\User\\snippets"
+    },
+    {
+        'fileName': 'Microsoft.PowerShell_profile.ps1', 
+        'windowsPathDir': lambda: os.environ["USERPROFILE"] + "\\Documents\\WindowsPowerShell"
+    },
+    {
+        'fileName': '.bashrc_aliases',
+        'linuxPathDir': lambda: os.environ["HOME"]
+    },
+    {
+        'fileName': '.zsh_original',
+        'linuxPathDir': lambda: os.environ["HOME"]
+    },
 ]
-
-currentDir = path.dirname(path.abspath(__file__))
+currentDir = os.path.dirname(os.path.abspath(__file__))
 
 def main():
     pf = platform.system()
     if(pf == 'Windows'):
-        pathProfile = os.environ["USERPROFILE"] + \
-            "\Documents\WindowsPowerShell"
-        files = [i for i in os.listdir(".") if not i in ignoreFiles]
-        ps1s = [i for i in files if i.split(".")[-1] == "ps1"]
-        # 戻り値が__NoneType__で例外処理とか増えていきそうなので、for句を使う。
-        for i in ps1s:
+        # windTargetDir = [n for n in targetFiles if n.get('windowsPathDir') is not None]
+        for n in targetFiles:
+            getPath = n.get('windowsPathDir')
+            isExistAttribute = getPath is not None
+            if (not isExistAttribute):
+                continue
+            path = getPath()
+            fileName = n.get('fileName')
             # 大半のWin環境はプロファイルディレクトリが無い。無ければ作る。
-            if not path.isdir(pathProfile):
-                os.mkdir(pathProfile)
+            if not os.path.isdir(path):
+                os.mkdir(path)
             log.debug(currentDir)
-            os.symlink(path.join(currentDir, i), path.join(pathProfile, i))
+            os.symlink(os.path.join(currentDir, fileName), os.path.join(path, fileName))
     elif(pf == 'Linux'):
-        pathProfile = os.environ["HOME"]
-        log.info(pf)
-        files = [i for i in os.listdir(".") if not i in ignoreFiles]
-        shellScripts = [i for i in files if not i.split(".")[-1] == "ps1"]
-        for i in shellScripts:
-            os.symlink(path.join(currentDir, i), path.join(pathProfile, i))
+        for n in targetFiles:
+            getPath = n.get('linuxPathDir')
+            isExistAttribute = getPath is not None
+            if (not isExistAttribute):
+                continue
+            path = getPath()
+            fileName = n.get('fileName')
+            # 大半のWin環境はプロファイルディレクトリが無い。無ければ作る。
+            if not os.path.isdir(path):
+                os.mkdir(path)
+            log.debug(currentDir)
+            os.symlink(os.path.join(currentDir, fileName), os.path.join(path, fileName))
     else:
         pass
 

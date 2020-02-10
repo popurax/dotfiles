b2943936578b2769fd6e35c4a8d7dbbb4d576d0c
TOTO <toto@gmail.com>
2020-02-10 19:00:52 +0900

    fix: Chocolateyの設定が自動で設定されていたので、追加。

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -57,3 +57,9 @@ function Start-RunAs
 }
 
 Set-Alias su Start-RunAs
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
